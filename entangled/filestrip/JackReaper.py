839d7b6cd65a593da75a1f9872bd0d9e90c138b4
Fernando Cezar <netcriptus@gmail.com>
2011-06-06 17:12:05 -0300

    Inseridos docstrings, a classe agora returna uma tupla (chave, valor) para cada pedaço do arquivo e para o descritor

--- entangled/filestrip/JackReaper.py
+++ entangled/filestrip/JackReaper.py
@@ -5,24 +5,35 @@ import hashlib
 from os.path import getsize
 
 class JackReaper(object):
+  """This class is supposed to tear a tear apart a large file into many 256kb
+  files, and creator a descriptor which gives us the names of each part"""
   def __init__(self):
     self.slice_size = 256*1024 #256 kb
     
 
+
+  def __getKey(self, value):
+    """Given a value, it will return a sha1 digest for that value"""
+    sha = hashlib.sha1()
+    sha.update(value)
+    return sha.hexdigest()
+
   def reap(self, arq):
+    """Given a file, it will be cut down to 256kb pieces. A descriptor will be
+    created and yielded"""
+    
     fp = open(arq,"r")
     size = getsize(arq)
     num_files = size / slice_size
     descriptor = ""
     
     for i in range(num_files+1):
-      piece = fp.read(slice_size)
-      yield piece
-      key = hashlib.sha1()
-      key.update(piece)
-      name=key.hexdigest()
-      descriptor += name + ".dat\n"
+      value = fp.read(slice_size)
+      key=self.__getKey(value)
+      yield (key, value)
+      descriptor += key + ".dat\n"
       
-    yield descriptor
+    key = self.__getKey(descriptor)
+    yield (key, descriptor)
   
   

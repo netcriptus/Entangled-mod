d8142dfbf1ac1f86c00a0a1412fbbb3a624ca206
Fernando Cezar <netcriptus@gmail.com>
2011-06-06 15:16:52 -0300

    Pythonizando scripts de separar e juntar arquivos

--- filestrip/join.py
+++ filestrip/join.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 import os
 
-slice = (240*1024)
+slice_size = 240*1024
 
 desc = open("musica.mp3.desc","r")
 
-name = desc.readline().rstrip()
+name = desc.readline().strip("\n")
 file_out = open("out_"+name,"w")
 
 lines = desc.readlines()
+desc.close()
 for line in lines:
-    file_tmp = open(line.rstrip(),"r")
+    file_tmp = open(line.strip("\n"),"r")
     piece = file_tmp.read()
     file_out.write(piece)
     file_tmp.close()
--- filestrip/strip.py
+++ filestrip/strip.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python
-import os
+# encoding: utf-8
+
 import hashlib
 from os.path import getsize
 
-slice = (240*1024)
+slice_size = 240*1024
 arq = "musica.mp3"
 
-file = open(arq,"r")
+fp = open(arq,"r")
 
 size = getsize(arq)
 
-num_files = size / (slice)
+num_files = size / slice_size
 
 descriptor = open(arq+".desc","w")
 descriptor.write(arq+"\n")
 
 for i in range(0,num_files+1):
-    piece = file.read(slice)
+    piece = fp.read(slice_size)
     key = hashlib.sha1()
     key.update(piece)
     name=key.hexdigest()
     descriptor.write(name+".dat\n")
-    print name
     new_file = open(str(name)+".dat","w")
     new_file.write(piece)
     new_file.close()
 
-file.close()
+fp.close()
 descriptor.close()
